85cb43c80a6447d6f2314c200bbb00c6fc07d3b6
sbarton237 <72349665+sbarton237@users.noreply.github.com>
2024-04-23 09:56:52 -0400

    Update C1.py

--- phase2/C1.py
+++ phase2/C1.py
@@ -2,9 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 arr = np.loadtxt("Phase 2\\amine_bp.csv", delimiter=",", skiprows=1)
+#https://numpy.org/doc/stable/reference/generated/numpy.loadtxt.html
 
 plt.plot([arr[i][0] for i in range(len(arr))], [arr[i][1] for i in range(len(arr))], marker="o", color="red", label=("primary"), linestyle='')
 plt.plot([arr[i][0] for i in range(len(arr))], [arr[i][2] for i in range(len(arr))], marker="^", color="green", label=("secondary"), linestyle='')
 plt.plot([arr[i][0] for i in range(len(arr))], [arr[i][3] for i in range(len(arr))], marker="d", color="blue", label=("tertiary"), linestyle='')
+#https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.plot.html
+
 plt.legend()
+#https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.legend.html
+
 plt.show()
+#https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.show.html
